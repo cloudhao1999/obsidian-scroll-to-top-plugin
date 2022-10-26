daa3daec33f6932709b924ebc805907fad7aa877
chen.yunhao <cyh19991002@gmail.com>
2022-10-26 16:25:34 +0800

    chore: 优化代码结构

--- main.ts
+++ main.ts
@@ -1,45 +1,55 @@
 import { MarkdownView, Plugin, ButtonComponent } from 'obsidian';
 
+const ROOT_WORKSPACE_CLASS = '.mod-vertical.mod-root'
+
 export default class MyPlugin extends Plugin {
+	// 最大尝试次数
+	maxValue: number = 100;
+	// 尝试次数
+	currentValue: number = 0;
 
 	private scroolToTop() {
 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 		if (markdownView) {
 			const editor = markdownView.editor;
-			if (editor) {
-				editor.scrollTo(0, 0);
-			}
 			const preview = markdownView.previewMode;
-			if (preview) {
-				preview.applyScroll(0);
 
-			}
+			editor && editor.scrollTo(0, 0);
+			preview && preview.applyScroll(0);
 		}
 	}
 
-	async onload() {
-		this.addCommand({
-			id: "scroll-to-top",
-			name: "Scroll to top",
-			callback: () => {
-				this.scroolToTop();
-			},
-		});
+	private createButton() {
+		this.currentValue++;
 
+		if (!document.body.querySelector(ROOT_WORKSPACE_CLASS)) {
+			// 尝试次数超过最大值，停止尝试
+			if (this.maxValue < this.currentValue) return
+			setTimeout(() => {
+				this.createButton();
+			}, 100)
+			return
+		}
 		// 创建一个圆形的按钮，在右下角
-		var topWidget = createEl("div");
+		let topWidget = createEl("div");
 		if (topWidget) {
 			topWidget.setAttribute("style", `position: absolute; bottom: 4.25em; right: 2em; z-index: 99;`);
 		}
 		topWidget.setAttribute("id", "scrollToTop");
-		var button = new ButtonComponent(topWidget);
+
+		let button = new ButtonComponent(topWidget);
 		button.setIcon("arrow-up").setClass('buttonItem').setTooltip('返回顶部').onClick(() => {
 			this.scroolToTop();
 		});
-		document.body
-			.querySelector(".mod-vertical.mod-root")
-			?.insertAdjacentElement("afterbegin", topWidget);
 
+		document.body
+			.querySelector(ROOT_WORKSPACE_CLASS)
+			?.insertAdjacentElement("afterbegin", topWidget);
+	}
+
+	async onload() {
+		this.currentValue = 0;
+		this.createButton();
 	}
 
 	onunload() {
