5c550b01975541882e914d6932710b4a51d75151
chen.yunhao <cyh19991002@gmail.com>
2022-10-27 11:40:40 +0800

    feat: add settings page (WIP) , add scroll bottom

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { MarkdownView, Plugin, ButtonComponent } from 'obsidian';
+import { MarkdownView, Plugin, ButtonComponent, PluginSettingTab, App, Setting } from 'obsidian';
+import { ScroolToTopSettingType, scroolToTopSetting } from './src/setting';
 
 const ROOT_WORKSPACE_CLASS = '.mod-vertical.mod-root'
 
@@ -7,6 +8,8 @@ export default class MyPlugin extends Plugin {
 	maxValue: number = 100;
 	// current try times
 	currentValue: number = 0;
+	// scrool to top settings
+	settings: ScroolToTopSettingType;
 
 	private scroolToTop() {
 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
@@ -14,13 +17,67 @@ export default class MyPlugin extends Plugin {
 			const editor = markdownView.editor;
 			const preview = markdownView.previewMode;
 
-			editor && editor.scrollTo(0, 0);
+			editor && editor.exec('goStart');
 			preview && preview.applyScroll(0);
 		}
 	}
 
+	private scroolToBottom() {
+		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (markdownView) {
+			const editor = markdownView.editor;
+			const preview = markdownView.previewMode;
+
+			editor && editor.exec('goEnd');
+			// the way to solve preview scroll to bottom
+			if (preview) {
+				let timer = setInterval(() => {
+					const prevScroll = preview.getScroll();
+					preview.applyScroll(preview.getScroll() + 5);
+					if (prevScroll === preview.getScroll()) {
+						clearInterval(timer);
+					}
+				})
+			}
+		}
+	}
+
+	private crateScrollElement(config: {
+		id: string,
+		bottom: number,
+		right: number,
+		icon: string,
+		tooltip: string,
+	}, fn: () => void) {
+		let topWidget = createEl("div");
+			if (topWidget) {
+				topWidget.setAttribute("style", `position: absolute; bottom: ${config.bottom}em; right: ${config.right}em; z-index: 99;`);
+			}
+			topWidget.setAttribute("id", config.id);
+
+			let button = new ButtonComponent(topWidget);
+			button.setIcon(config.icon).setClass('buttonItem').setTooltip(config.tooltip).onClick(() => {
+				fn()
+			});
+
+			document.body
+				.querySelector(ROOT_WORKSPACE_CLASS)
+				?.insertAdjacentElement("afterbegin", topWidget);
+
+			document.addEventListener("click", function (event) {
+				const activeLeaf = app.workspace.getActiveViewOfType(MarkdownView);
+				if (activeLeaf) {
+					topWidget.style.visibility = "visible";
+				} else {
+					topWidget.style.visibility = "hidden";
+				}
+			});
+	}
+
 	private createButton() {
 		this.currentValue++;
+
+		const { enabledScroolToTop, enabledScroolToBottom } = this.settings
 
 		if (!document.body.querySelector(ROOT_WORKSPACE_CLASS)) {
 			// stop when reach max try times
@@ -30,38 +87,84 @@ export default class MyPlugin extends Plugin {
 			}, 100)
 			return
 		}
-		// create a button
-		let topWidget = createEl("div");
-		if (topWidget) {
-			topWidget.setAttribute("style", `position: absolute; bottom: 4.25em; right: 2em; z-index: 99;`);
+		if (enabledScroolToTop) {
+			// create a button
+			this.crateScrollElement({
+				id: 'scrollToTop',
+				bottom: 5.75,
+				right: 2,
+				icon: 'arrow-up',
+				tooltip: 'Scroll to top',
+			}, this.scroolToTop.bind(this))
 		}
-		topWidget.setAttribute("id", "scrollToTop");
 
-		let button = new ButtonComponent(topWidget);
-		button.setIcon("arrow-up").setClass('buttonItem').setTooltip('返回顶部').onClick(() => {
-			this.scroolToTop();
-		});
+		if (enabledScroolToBottom) {
+			this.crateScrollElement({
+				id: 'scrollToBottom',
+				bottom: 2.65,
+				right: 2,
+				icon: 'arrow-down',
+				tooltip: 'Scroll to Bottom',
+			}, this.scroolToBottom.bind(this))
+		}
 
-		document.body
-			.querySelector(ROOT_WORKSPACE_CLASS)
-			?.insertAdjacentElement("afterbegin", topWidget);
-
-		document.addEventListener("click", function (event) {
-			const activeLeaf = app.workspace.getActiveViewOfType(MarkdownView);
-			if (activeLeaf) {
-				topWidget.style.visibility = "visible";
-			} else {
-				topWidget.style.visibility = "hidden";
-			}
-		});
 	}
 
 	async onload() {
+		await this.loadSettings();
+		this.addSettingTab(new ScrollToTopSettingTab(this.app, this));
 		this.currentValue = 0;
 		this.createButton();
 	}
 
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, scroolToTopSetting, await this.loadData());
+	}
+
 	onunload() {
 
+	}
+}
+
+class ScrollToTopSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		containerEl.createEl('h2', { text: 'Scrool To Top Settings' });
+
+		new Setting(containerEl)
+			.setName('Show scroll to top button')
+			.setDesc('Show scroll to top button in the right bottom corner.')
+			.addToggle(value => {
+				value.setValue(this.plugin.settings.enabledScroolToTop)
+					.onChange(async (value) => {
+						this.plugin.settings.enabledScroolToTop = value;
+						await this.plugin.saveSettings();
+					});
+			})
+
+		new Setting(containerEl)
+			.setName('Show scroll to bottom button')
+			.setDesc('Show scroll to bottom button in the right bottom corner.')
+			.addToggle(value => {
+				value.setValue(this.plugin.settings.enabledScroolToBottom)
+					.onChange(async (value) => {
+						this.plugin.settings.enabledScroolToBottom = value;
+						await this.plugin.saveSettings();
+					});
+			})
 	}
 }
---
+++ src/setting.ts
@@ -0,0 +1,9 @@
+export interface ScroolToTopSettingType {
+	enabledScroolToTop: boolean;
+	enabledScroolToBottom: boolean;
+}
+
+export const scroolToTopSetting: ScroolToTopSettingType = {
+	enabledScroolToTop: true,
+	enabledScroolToBottom: true,
+}
